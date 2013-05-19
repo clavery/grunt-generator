747b5b6450ed55ba160971a29cb1256bba2b138c
Charles Lavery <charles.lavery@gmail.com>
2013-05-19 16:21:31 -0400

    updates jshint and jasmine-node packages
    fixes bugs with updated packages

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,7 +36,6 @@ module.exports = function(grunt) {
         boss: true,
         eqnull: true,
         node: true,
-        es5: true,
         globals: {
           // jasmine globals
           describe: false,
